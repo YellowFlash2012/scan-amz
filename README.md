5b5ea5f4f4ed28a2c48bc366616c66c075656062
YellowFlash2012 <cyclop9@hotmail.com>
2022-04-05 06:38:28 +0100

    updated the readme file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # scan-amz
 Some ecom UI built with reactjs
+
+# PLP
+![scan-amz](/client/public/PLP.png)
+
+# PDP
+![scan-amz](/client/public/PDP.png)
--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -11,7 +11,7 @@ import { useState } from "react";
 
 const Product = () => {
 
-    const navigate = useNavigate();
+    
     const dispatch = useDispatch();
 
     let { id } = useParams();
